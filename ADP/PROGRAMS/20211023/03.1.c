adc968436e76e0f6a990bd14c3d489ad464b36d1
Kaushal Bhatol <kaushal.bhatol@outlook.com>
2021-10-25 10:06:02 +0530

    UPDATE

--- ADP/PROGRAMS/20211023/03.1.c
+++ ADP/PROGRAMS/20211023/03.1.c
@@ -3,13 +3,25 @@
     and MERGE into third array.use 1-D array.
 */
 
+/*
+    -Write a C program to get two arrays from user
+    and MERGE into third array.use 1-D array.
+*/
+
 #include <stdio.h>
 void main()
 {
-    int i, a[5], b[5], c[10], cnt = 0;
+    int a[5], b[5], c[10], i, cnt = 0;
+
     for (i = 0; i < 5; i++)
     {
-        scanf("%d %d", &a[i], &b[i]);
+        printf("enter a[%d] = ", i);
+        scanf("%d", &a[i]);
+    }
+    for (i = 0; i < 5; i++)
+    {
+        printf("enter b[%d] = ", i);
+        scanf("%d", &b[i]);
     }
 
     for (i = 0; i < 5; i++)
@@ -22,6 +34,7 @@ void main()
         c[cnt] = b[i];
         cnt++;
     }
+
     for (i = 0; i < 10; i++)
     {
         printf("%d ", c[i]);
