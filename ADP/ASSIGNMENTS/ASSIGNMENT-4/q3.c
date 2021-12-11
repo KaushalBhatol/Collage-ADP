e0ab7979ef56f4a40cd88e07763eeff230df959f
KaushalBhatol <kaushal.bhatol@outlook.com>
2021-12-11 18:49:34 +0530

    assignment-4

---
+++ ADP/ASSIGNMENTS/ASSIGNMENT-4/q3.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+void main()
+{
+    int a[2][2], b[2][2], i, j;
+    for (i = 0; i < 2; i++) // getting both arrays value from user
+    {
+        for (j = 0; j < 2; j++)
+        {
+            printf("Enter value of a[%d][%d] : ", i, j);
+            scanf("%d", &a[i][j]);
+            printf("Enter value of b[%d][%d] : ", i, j);
+            scanf("%d", &b[i][j]);
+        }
+    }
+    printf("addition of two loop is\n");
+    for (i = 0; i < 2; i++) // Printing sum of two arrays
+    {
+        for (j = 0; j < 2; j++)
+        {
+            printf("%d + %d = %d \n", a[i][j], b[i][j], a[i][j] + b[i][j]);
+        }
+    }
+    printf("Substracrion of two loop is\n");
+    for (i = 0; i < 2; i++) // Printing sum of two arrays
+    {
+        for (j = 0; j < 2; j++)
+        {
+            printf("%d - %d = %d \n", a[i][j], b[i][j], a[i][j] - b[i][j]);
+        }
+    }
+    printf("Multiplication of two loop is\n");
+    for (i = 0; i < 2; i++) // Printing sum of two arrays
+    {
+        for (j = 0; j < 2; j++)
+        {
+            printf("%d * %d = %d \n", a[i][j], b[i][j], a[i][j] * b[i][j]);
+        }
+    }
+    printf("division of two loop is\n");
+    for (i = 0; i < 2; i++) // Printing sum of two arrays
+    {
+        for (j = 0; j < 2; j++)
+        {
+            printf("%d / %d = %d \n", a[i][j], b[i][j], a[i][j] / b[i][j]);
+        }
+    }
+}
